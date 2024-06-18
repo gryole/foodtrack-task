a557607ec7fc1e505410d2e70356ecff7d79499b
ogrytsenko <okulele.gry@gmail.com>
2024-06-18 15:16:29 +0200

    Add tests for parsing data

---
+++ src/test/java/com/foodtruck/api/service/LoadDataServiceTest.java
@@ -0,0 +1,43 @@
+package com.foodtruck.api.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.foodtruck.api.model.FoodTrack;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+@ExtendWith(MockitoExtension.class)
+class LoadDataServiceTest {
+  private final LoadDataService loadDataService = new LoadDataService();
+
+  @BeforeEach
+  void setUp() {
+    ReflectionTestUtils.setField(loadDataService, "dataFile", "test_data.csv");
+  }
+
+  @Test
+  void shouldParseData() {
+    List<FoodTrack> foodTracks = loadDataService.parseData();
+
+    assertThat(foodTracks)
+        .contains(
+            FoodTrack.builder()
+                .locationId("735318")
+                .applicant("Ziaurehman Amini")
+                .facilityType("Push Cart")
+                .locationDescription("MARKET ST: DRUMM ST intersection")
+                .address("5 THE EMBARCADERO")
+                .status("REQUESTED")
+                .foodItems("South American/Peruvian food")
+                .latitude("37.7943310032468")
+                .longitude("-122.395811053023")
+                .schedule(
+                    "http://bsm.sfdpw.org/PermitsTracker/reports/report.aspx?title=schedule&report=rptSchedule&params=permit=15MFF-0159&ExportPDF=1&Filename=15MFF-0159_schedule.pdf")
+                .daysHours("Mo-We:7AM-7PM")
+                .build());
+  }
+}
